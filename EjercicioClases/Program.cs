c5e4db865712c5561039c376c3875ada8c2d16df
andres-rivera-05 <cristianrivera161141@gmail.com>
2023-11-05 17:56:41 -0600

    creando primer objeto
    creando el primer superHeroe

--- EjercicioClases/Program.cs
+++ EjercicioClases/Program.cs
@@ -1,3 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+
+SuperHeroe superHeroe = new SuperHeroe();
+
+superHeroe.Nombre = "superman";
+superHeroe.IdentidadSecreta = "Clark Kent";
+superHeroe.Ciudad = "Metropolis";
+superHeroe.PuedeVolar = true;
+superHeroe.superPoder.Nombre = "Super Fuerza";
+superHeroe.descripcion.Descripcion = "Cpacidad de levantar objetos muy pesados";
+superHeroe.nivel.Nivel = 9;
+
+
+public class SuperHeroe { 
+    public string Nombre {  get; set; } 
+    public string IdentidadSecreta { get; set; }    
+    public string Ciudad {  get; set; } 
+    public bool PuedeVolar {  get; set; }   
+    public SuperPoder superPoder { get; set; }
+    public SuperPoder descripcion { get; set; }
+    public SuperPoder nivel { get; set; }
+
+}
+
+public class SuperPoder {
+    public string Nombre { get; set; } 
+    public string Descripcion { get; set; } 
+    public int Nivel {  get; set; }
+}
