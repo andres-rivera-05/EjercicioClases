eb2549bd7c6425050d8528e6e9b49d348b200b6b
andres-rivera-05 <cristianrivera161141@gmail.com>
2023-11-05 18:14:10 -0600

    agregando constructor

--- EjercicioClases/Program.cs
+++ EjercicioClases/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿
 
 SuperHeroe superHeroe = new SuperHeroe();
 
@@ -8,24 +6,32 @@ superHeroe.Nombre = "superman";
 superHeroe.IdentidadSecreta = "Clark Kent";
 superHeroe.Ciudad = "Metropolis";
 superHeroe.PuedeVolar = true;
-superHeroe.superPoder.Nombre = "Super Fuerza";
-superHeroe.descripcion.Descripcion = "Cpacidad de levantar objetos muy pesados";
-superHeroe.nivel.Nivel = 9;
+superHeroe.superPoder.Nombre = "SuperFuerza";
+superHeroe.superPoder.Descripcion = "Cpacidad de levantar objetos muy pesados";
+
+
+Console.WriteLine($"Nombre: {superHeroe.Nombre}");
+Console.WriteLine($"Identida Secreta: {superHeroe.IdentidadSecreta}");
+Console.WriteLine($"Ciudad: {superHeroe.Ciudad}");
+Console.WriteLine($"PuedeVolar: {superHeroe.PuedeVolar}");
+Console.WriteLine($"Super Poder: {superHeroe.superPoder.Nombre}");
 
 
 public class SuperHeroe { 
-    public string Nombre {  get; set; } 
-    public string IdentidadSecreta { get; set; }    
-    public string Ciudad {  get; set; } 
+    public string? Nombre {  get; set; } 
+    public string? IdentidadSecreta { get; set; }    
+    public string? Ciudad {  get; set; } 
     public bool PuedeVolar {  get; set; }   
     public SuperPoder superPoder { get; set; }
-    public SuperPoder descripcion { get; set; }
-    public SuperPoder nivel { get; set; }
+
+    public SuperHeroe() {
+        superPoder = new SuperPoder();
+    }
 
 }
 
 public class SuperPoder {
-    public string Nombre { get; set; } 
-    public string Descripcion { get; set; } 
+    public string? Nombre { get; set; } 
+    public string? Descripcion { get; set; } 
     public int Nivel {  get; set; }
 }
